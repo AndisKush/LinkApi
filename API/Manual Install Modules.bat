c3bccf67ccde04dbb6162f260d1079618e7e0e69
Anderson <anderson.ojunior@gmail.com>
2023-02-14 16:17:28 -0300

    linkapi

--- API/Manual Install Modules.bat
+++ API/Manual Install Modules.bat
@@ -1,6 +1,5 @@
-call npm install express@4.18.2
-call npm install cors@2.8.5
-call npm install libphonenumber-js@1.10.14
-call npm install venom-bot@4.3.0
-call npm install node-firebird@1.0.0
 call npm install node-windows@1.0.0-beta.8
+call npm install
+timeout 3
+call fixLib.bat
+timeout 3
