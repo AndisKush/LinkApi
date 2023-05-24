58221315011a414157d85206ca1119a3f09c397c
Anderson <anderson.ojunior@gmail.com>
2023-05-24 09:15:51 -0300

    fixlib

--- API/fixLib.bat
+++ API/fixLib.bat
@@ -1,2 +1,2 @@
-move /y "retriever.layer.js" "node_modules/venom-bot/dist/api/layers/"
-move /y "Injected.js" "node_modules/venom-bot/dist/api/layers/"
+move /y "Injected.js" "node_modules/whatsapp-web.js/src/util/"
+move /y "Message.js" "node_modules/whatsapp-web.js/src/structures/"
